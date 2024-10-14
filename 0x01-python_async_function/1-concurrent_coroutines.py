5c0a9be4a566e7cabdcdc520e9de1fb9d3d742d4
Kimathi isaac <Isaackimathi@gmail.com>
2024-10-14 14:13:00 +0300

    asynchronous coroutine

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Module executes wait_random n times
+"""
 
 import asyncio
 from typing import List
@@ -6,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """returns list of floats"""
+    '''Returns a list floats'''
     wait_times = await asyncio.gather(
             *tuple(map(lambda _: wait_random(max_delay), range(n)))
             )
