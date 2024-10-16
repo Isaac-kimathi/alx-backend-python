6921feac8dad64ac5a726ab4cae99afcb5828fab
Kimathi isaac <Isaackimathi@gmail.com>
2024-10-16 12:40:33 +0300

    type-annotated function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""module for type-annotated function add"""
+
+def add(a:float, b:float) -> float:
+    """function takes  floats as arguments and returns their sum as float"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,5 @@
+Type annotations in Python 3
+use type annotations to specify function signatures and variable types
+Duck typing
+Validation of code with mypy
+pip install mypy
