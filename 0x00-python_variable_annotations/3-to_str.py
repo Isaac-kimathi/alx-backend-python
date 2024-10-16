a64e1e9cd0d04872445246cb7b7b0c0725bd9b8c
Kimathi isaac <Isaackimathi@gmail.com>
2024-10-16 13:53:02 +0300

    type-annotated function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""module for type-annotated function"""
+
+def to_str(n: float) -> str:
+    """function returns the string representation of the float"""
+    return str(n)
