e92c1d8f67002fa7b5a1c60d7b760fc8fae8c559
Kimathi isaac <Isaackimathi@gmail.com>
2024-10-17 13:30:21 +0300

    multiplier function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """module for type-annotated function"""
+from typing import Iterable, List, Sequence, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Function that takes in arguments of iterable sequence"""
